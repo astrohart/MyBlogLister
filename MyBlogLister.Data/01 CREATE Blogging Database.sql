d08e502677e66e580af86204b6d130cde1661827
Brian Hart <brian.hart@xylogix.net>
2020-12-28 15:20:49 -0500

    Create Blogging database create script

---
+++ MyBlogLister.Data/01 CREATE Blogging Database.sql
@@ -0,0 +1,28 @@
+﻿ALTER TABLE [dbo].[Posts] DROP CONSTRAINT [FK_Posts_Blogs];
+
+DROP TABLE [dbo].[Posts];
+
+DROP TABLE [dbo].[Blogs];
+
+CREATE TABLE [dbo].[Blogs] (
+    [BlogId]    INT            IDENTITY (1, 1) NOT NULL,
+    [Author]    NVARCHAR (255) NOT NULL,
+    [Date]      NVARCHAR (255) NOT NULL,
+    [Name]      NVARCHAR (255) NOT NULL,
+    [Permalink] NVARCHAR (255) NOT NULL,
+    PRIMARY KEY CLUSTERED ([BlogId] ASC),
+    UNIQUE NONCLUSTERED ([BlogId] ASC)
+);
+
+CREATE TABLE [dbo].[Posts] (
+    [PostId]    INT            IDENTITY (1, 1) NOT NULL,
+    [BlogId]    INT            NOT NULL,
+    [Title]     NVARCHAR (255) NOT NULL,
+    [Date]      NVARCHAR (255) NOT NULL,
+    [Permalink] NVARCHAR (255) NOT NULL,
+    [Content]   NVARCHAR (255) NOT NULL,
+    PRIMARY KEY CLUSTERED ([PostId] ASC),
+    UNIQUE NONCLUSTERED ([PostId] ASC),
+    CONSTRAINT [FK_Posts_Blogs] FOREIGN KEY ([BlogId]) REFERENCES [dbo].[Blogs] ([BlogId])
+);
+
