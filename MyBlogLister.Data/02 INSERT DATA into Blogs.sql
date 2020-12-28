7887b6fc9a378e200021e7b55c8ef7eccd69c2dd
Brian Hart <brian.hart@xylogix.net>
2020-12-28 15:29:53 -0500

    Create 02 INSERT DATA into Blogs.sql

---
+++ MyBlogLister.Data/02 INSERT DATA into Blogs.sql
@@ -0,0 +1,4 @@
+﻿SET IDENTITY_INSERT [dbo].[Blogs] ON
+INSERT INTO [dbo].[Blogs] ([BlogId], [Author], [Date], [Name], [Permalink]) VALUES (1, N'astroguy343', N'2020-12-28T13:37:47-0400', N'Outer Space Factoids', N'https://localhost:4954/blogs/outer-space-factoids/')
+INSERT INTO [dbo].[Blogs] ([BlogId], [Author], [Date], [Name], [Permalink]) VALUES (2, N'astroguy343', N'2020-12-28T11:07:49-0400', N'Blog About my Dog', N'https://localhost:4954/blogs/blog-about-my-dog/')
+SET IDENTITY_INSERT [dbo].[Blogs] OFF
