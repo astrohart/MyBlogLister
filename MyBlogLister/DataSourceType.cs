cfe5ea5ea608251c94bd1112ca886c4b43d4e10d
Brian Hart <brian.hart@xylogix.net>
2020-12-29 18:39:01 -0500

    Add docs to the DataSourceType enum

--- MyBlogLister/DataSourceType.cs
+++ MyBlogLister/DataSourceType.cs
@@ -1,8 +1,18 @@
 ﻿namespace MyBlogLister
 {
+    /// <summary>
+    /// Type of data source from which the application obtains configuration.
+    /// </summary>
     public enum DataSourceType
     {
+        /// <summary>
+        /// Configuration information is to be obtained from a database.
+        /// </summary>
         Database,
+
+        /// <summary>
+        /// Configuration information is to be obtained from a JSON-formatted file.
+        /// </summary>
         JsonFile
     }
 }
